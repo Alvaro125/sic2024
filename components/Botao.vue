19aad802b5c23e6fb7cf019190293362ae768b65
Álvaro Martinez Ferreira <alvaromartinezferreira@gmail.com>
2024-10-02 20:08:46 -0300

    aula 1

---
+++ components/Botao.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="click" ><h1>{{ props.text }}</h1>
+    <slot/> </button>
+</template>
+<script setup lang="ts">
+const model = defineModel()
+const props = defineProps<{
+    text: string
+}>()
+
+const {data:photos, execute, error, refresh, status} = await useFetch('https://jsonplaceholder.typicode.com/photos',{
+        method: 'GET'
+    })
+async function click(event: EventTarget){
+    // const data = await $fetch('https://jsonplaceholder.typicode.com/photos',{
+    //     method: 'GET'
+    // })
+    // model.value = data
+    console.log(photos)
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="test">
+    <h1>{{grandetexto}}</h1>
+    <input type="text" name="" id="" v-model="text">
+
+    <input type="number" v-model="n1">
+    <input type="number" v-model="n2">
+    <Botao :text="text" v-model="data"><small>aqui</small></Botao>
+    {{ data }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const text = ref<string>("Hello World 1")
+const data = ref()
+const grandetexto = computed(()=>{
+  return text.value.toUpperCase()
+})
+
+const n1 = ref<number>(0)
+const n2 = ref<number>(0)
+
+watch([n1,n2],(now,before)=>{
+  if(n1.value+n2.value>50){
+    text.value = "A balaça não aguenta, reduza o peso"
+  }else{
+    text.value = "ok"
+  }
+})
+</script>
+
+<style>
+#test{
+  display: flex;
+  flex-direction: column;
+}
+</style>
